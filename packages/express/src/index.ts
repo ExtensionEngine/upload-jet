fb3b96e3c032a1caaafff69d139f8b65d489c10f
Petar Trutanic <ptrutanic@extensionengine.com>
2023-07-28 11:03:20 +0200

    Added body parsing

--- packages/express/src/index.ts
+++ packages/express/src/index.ts
@@ -1,5 +1,6 @@
 import { UploadJetConfig, UploadRouteConfig } from 'types';
 import axios from 'axios';
+import * as express from 'express';
 
 export class UploadJet {
   #apiKey: string;
@@ -9,33 +10,35 @@ export class UploadJet {
   }
 
   createUploadRoute(config: UploadRouteConfig) {
-    return async (req: any, res: any, next: any) => {
-      const fileNames = req.body.fileNames;
+    return (req: any, res: any, next: any) => {
+      return express.json()(req, res, async () => {
+        const fileNames = req.body.fileNames;
 
-      if (!fileNames || !fileNames.length) {
-        throw new Error('Missing file names');
-      }
+        if (!fileNames || !fileNames.length) {
+          throw new Error('Missing file names');
+        }
 
-      const policyData = {};
-      fileNames.forEach((name: string) => {
-        const fileName = config.setFileName
-          ? config.setFileName(req, name)
-          : name;
+        const policyData = {};
+        fileNames.forEach((name: string) => {
+          const fileName = config.setFileName
+            ? config.setFileName(req, name)
+            : name;
 
-        policyData[name] = {
-          key: fileName,
-          maxFileSize: config.maxFileSize,
-          fileType: config.fileType,
-          public: config.public
-        };
+          policyData[name] = {
+            key: fileName,
+            maxFileSize: config.maxFileSize,
+            fileType: config.fileType,
+            public: config.public
+          };
+        });
+
+        const response = await axios.post(
+          'http://localhost:3000/upload-policy',
+          policyData
+        );
+
+        return res.json(response.data);
       });
-
-      const response = await axios.post(
-        'http://localhost:3000/upload-policy',
-        policyData
-      );
-
-      return res.json(response.data);
     };
   }
 }
