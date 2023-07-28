23378c7e0c7ee43a9ef25049afd8bd0da47d5cf7
Petar Trutanic <ptrutanic@extensionengine.com>
2023-07-28 09:34:52 +0200

    Added create upload route

--- packages/express/src/index.ts
+++ packages/express/src/index.ts
@@ -1,4 +1,6 @@
-import { UploadJetConfig } from 'types';
+import { UploadJetConfig, UploadRouteConfig } from 'types';
+import axios from 'axios';
+import express, { Request } from 'express';
 
 export class UploadJet {
   #apiKey: string;
@@ -7,7 +9,34 @@ export class UploadJet {
     this.#apiKey = config.apiKey;
   }
 
-  getApiKey() {
-    return this.#apiKey;
+  createUploadRoute(config: UploadRouteConfig) {
+    return async (req: Request, res: any, next: Function) => {
+      const fileNames = req.body.fileNames;
+
+      if (!fileNames || !fileNames.length) {
+        throw new Error('Missing file names');
+      }
+
+      const policyData = {};
+      fileNames.forEach((name: string) => {
+        const fileName = config.setFileName
+          ? config.setFileName(req, name)
+          : name;
+
+        policyData[name] = {
+          key: fileName,
+          maxFileSize: config.maxFileSize,
+          fileType: config.fileType,
+          public: config.public
+        };
+      });
+
+      const response = await axios.post(
+        'http://localhost:3000/upload-policy',
+        policyData
+      );
+
+      return res.json(response.data);
+    };
   }
 }
--- packages/express/src/types.ts
+++ packages/express/src/types.ts
@@ -1,3 +1,10 @@
 export type UploadJetConfig = {
   apiKey: string;
 };
+
+export type UploadRouteConfig = {
+  fileType: string;
+  maxFileSize: number;
+  public: boolean;
+  setFileName: Function;
+};
