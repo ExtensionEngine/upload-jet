a7fddc4479b4df72b49029e49de68c043f611f5e
Marko1310 <marko.cabo13@gmail.com>
2023-07-28 16:47:54 +0200

    Init vue project, basic file structure, add basic styling

---
+++ packages/vue/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import UploadJet from './components/UploadJet.vue'
+</script>
+
+<template>
+  <UploadJet />
+</template>
+
+<style scoped></style>
---
+++ packages/vue/src/components/UploadJet.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="dropzone">
+    <div class="dropzone-content">
+      <p class="dropzone-content-message">Drag and drop the file you want to upload here</p>
+      <label for="fileInput" class="browse-label"
+        >Or, <span class="browse-link">browse your file</span></label
+      >
+      <input type="file" id="fileInput" class="file-input" />
+    </div>
+  </div>
+</template>
+
+<style>
+.dropzone {
+  border: dashed;
+  padding: 3rem 6rem;
+  border-radius: 5px;
+  border-color: rgb(255, 255, 255);
+  background-color: rgb(28, 37, 48);
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.dropzone-content {
+  text-align: center;
+}
+
+.dropzone-content-browse {
+  margin-top: 0.5rem;
+}
+
+.browse-label {
+  cursor: pointer;
+  margin-bottom: 10px;
+}
+
+.browse-link {
+  text-decoration: underline;
+}
+
+.file-input {
+  display: none;
+}
+</style>
