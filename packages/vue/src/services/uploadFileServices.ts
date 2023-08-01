3ff8a39c3092cd0888284507cbef36ddc686b637
Marko1310 <marko.cabo13@gmail.com>
2023-08-01 15:48:46 +0200

    Implement fetch instead of axios

--- packages/vue/src/services/uploadFileServices.ts
+++ packages/vue/src/services/uploadFileServices.ts
@@ -1,5 +1,3 @@
-import axios from 'axios';
-
 const getPostPolicy = async function (url: string, fileName: string[]) {
   // mock a return value -> post policies
 
@@ -13,12 +11,12 @@ const getPostPolicy = async function (url: string, fileName: string[]) {
         'X-Amz-Algorithm': 'AWS4-HMAC-SHA256',
         'X-Amz-Credential':
           'AKIA3O56TMPDARXIXBWQ/20230801/eu-north-1/s3/aws4_request',
-        'X-Amz-Date': '20230801T123705Z',
+        'X-Amz-Date': '20230801T134500Z',
         key: 'file1',
         Policy:
-          'eyJleHBpcmF0aW9uIjoiMjAyMy0wOC0wMVQxMzozNzowNVoiLCJjb25kaXRpb25zIjpbWyJjb250ZW50LWxlbmd0aC1yYW5nZSIsMCwyNTAwMDAwXSx7IlRhZ2dpbmciOiI8VGFnZ2luZz48VGFnU2V0PjxUYWc+PEtleT5wb2xpY3k8L0tleT48VmFsdWU+cHVibGljPC9WYWx1ZT48L1RhZz48L1RhZ1NldD48L1RhZ2dpbmc+In0seyJidWNrZXQiOiJtY2FibyJ9LHsiWC1BbXotQWxnb3JpdGhtIjoiQVdTNC1ITUFDLVNIQTI1NiJ9LHsiWC1BbXotQ3JlZGVudGlhbCI6IkFLSUEzTzU2VE1QREFSWElYQldRLzIwMjMwODAxL2V1LW5vcnRoLTEvczMvYXdzNF9yZXF1ZXN0In0seyJYLUFtei1EYXRlIjoiMjAyMzA4MDFUMTIzNzA1WiJ9LHsia2V5IjoiZmlsZTEifV19',
+          'eyJleHBpcmF0aW9uIjoiMjAyMy0wOC0wMVQxNDo0NTowMFoiLCJjb25kaXRpb25zIjpbWyJjb250ZW50LWxlbmd0aC1yYW5nZSIsMCwyNTAwMDAwXSx7IlRhZ2dpbmciOiI8VGFnZ2luZz48VGFnU2V0PjxUYWc+PEtleT5wb2xpY3k8L0tleT48VmFsdWU+cHVibGljPC9WYWx1ZT48L1RhZz48L1RhZ1NldD48L1RhZ2dpbmc+In0seyJidWNrZXQiOiJtY2FibyJ9LHsiWC1BbXotQWxnb3JpdGhtIjoiQVdTNC1ITUFDLVNIQTI1NiJ9LHsiWC1BbXotQ3JlZGVudGlhbCI6IkFLSUEzTzU2VE1QREFSWElYQldRLzIwMjMwODAxL2V1LW5vcnRoLTEvczMvYXdzNF9yZXF1ZXN0In0seyJYLUFtei1EYXRlIjoiMjAyMzA4MDFUMTM0NTAwWiJ9LHsia2V5IjoiZmlsZTEifV19',
         'X-Amz-Signature':
-          'd0304ffc45613e33d26e55191d5d0c01ff62495b07d2601a1dc000a195334a0f'
+          '68c10958509c8bfa1e27dac08d80e38a6eae416ddacc3bb12a36172c0714507d'
       }
     },
     'image.png': {
@@ -30,24 +28,22 @@ const getPostPolicy = async function (url: string, fileName: string[]) {
         'X-Amz-Algorithm': 'AWS4-HMAC-SHA256',
         'X-Amz-Credential':
           'AKIA3O56TMPDARXIXBWQ/20230801/eu-north-1/s3/aws4_request',
-        'X-Amz-Date': '20230801T123705Z',
+        'X-Amz-Date': '20230801T134500Z',
         key: 'file2',
         Policy:
-          'eyJleHBpcmF0aW9uIjoiMjAyMy0wOC0wMVQxMzozNzowNVoiLCJjb25kaXRpb25zIjpbWyJjb250ZW50LWxlbmd0aC1yYW5nZSIsMCwyNTAwMDAwXSx7IlRhZ2dpbmciOiI8VGFnZ2luZz48VGFnU2V0PjxUYWc+PEtleT5wb2xpY3k8L0tleT48VmFsdWU+cHVibGljPC9WYWx1ZT48L1RhZz48L1RhZ1NldD48L1RhZ2dpbmc+In0seyJidWNrZXQiOiJtY2FibyJ9LHsiWC1BbXotQWxnb3JpdGhtIjoiQVdTNC1ITUFDLVNIQTI1NiJ9LHsiWC1BbXotQ3JlZGVudGlhbCI6IkFLSUEzTzU2VE1QREFSWElYQldRLzIwMjMwODAxL2V1LW5vcnRoLTEvczMvYXdzNF9yZXF1ZXN0In0seyJYLUFtei1EYXRlIjoiMjAyMzA4MDFUMTIzNzA1WiJ9LHsia2V5IjoiZmlsZTIifV19',
+          'eyJleHBpcmF0aW9uIjoiMjAyMy0wOC0wMVQxNDo0NTowMFoiLCJjb25kaXRpb25zIjpbWyJjb250ZW50LWxlbmd0aC1yYW5nZSIsMCwyNTAwMDAwXSx7IlRhZ2dpbmciOiI8VGFnZ2luZz48VGFnU2V0PjxUYWc+PEtleT5wb2xpY3k8L0tleT48VmFsdWU+cHVibGljPC9WYWx1ZT48L1RhZz48L1RhZ1NldD48L1RhZ2dpbmc+In0seyJidWNrZXQiOiJtY2FibyJ9LHsiWC1BbXotQWxnb3JpdGhtIjoiQVdTNC1ITUFDLVNIQTI1NiJ9LHsiWC1BbXotQ3JlZGVudGlhbCI6IkFLSUEzTzU2VE1QREFSWElYQldRLzIwMjMwODAxL2V1LW5vcnRoLTEvczMvYXdzNF9yZXF1ZXN0In0seyJYLUFtei1EYXRlIjoiMjAyMzA4MDFUMTM0NTAwWiJ9LHsia2V5IjoiZmlsZTIifV19',
         'X-Amz-Signature':
-          'fb1dea0c34e5001fdb7d14e269e1b4ac47f48d957ee47ca40442809961cb99ee'
+          '14b3fb80a8774a95f0a93fe87bb111dfac911c67ea6c6159101fa2ac726b2a39'
       }
     }
   };
 };
 
 const postFileToAWS = async function (url: string, formData: FormData) {
-  const response = await axios.post(url, formData, {
-    headers: {
-      'Content-Type': 'multipart/form-data'
-    }
+  return fetch(url, {
+    method: 'POST',
+    body: formData
   });
-  return response.data;
 };
 
 export default { getPostPolicy, postFileToAWS };
