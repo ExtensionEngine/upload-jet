669e6ffe1a1a0266df750865353bfe430c713dda
Petar Trutanic <ptrutanic@extensionengine.com>
2023-08-02 09:57:16 +0200

    Added example express module app

---
+++ packages/express/example-app/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import { UploadJet } from 'upload-jet-express';
+
+const app = express();
+const PORT = 3001;
+const API_KEY = 'example-api-key';
+
+const uploadJet = new UploadJet({ apiKey: API_KEY });
+const uploadRouteConfig = {
+  fileType: 'image',
+  maxFileSize: '1MB',
+  public: true,
+  setFileName: (req, fileName) => {
+    return fileName;
+  }
+};
+
+app.use('/api/example', uploadJet.createUploadRoute(uploadRouteConfig));
+
+app.listen(PORT, () => {
+  console.log('App is listening on port ' + PORT);
+});
