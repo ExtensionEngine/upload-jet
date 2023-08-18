350b9ff715acb7fca5509b63f39e0f5ff3b9cd2a
Marko1310 <marko.cabo13@gmail.com>
2023-08-18 14:05:42 +0200

    Basic page layout

---
+++ webapp/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="grid h-screen grid-cols-5 bg-slate-200">
+    <div class="col-span-1 border-2 border-red-400">Sidebar</div>
+    <div class="col-span-4 border-2 border-red-400">Main Content</div>
+    <!-- <slot /> -->
+  </div>
+</template>
+
+<style scoped></style>
+
+<div class="col-span-4 bg-white"></div>
---
+++ webapp/pages/about.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>aaaaa</div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'default'
+});
+</script>
+
+<style lang="scss" scoped></style>
