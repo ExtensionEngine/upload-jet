a3a14d24c8aa0f1c15f02e721bd9c55b8c736c45
Marko1310 <marko.cabo13@gmail.com>
2023-08-21 17:27:19 +0200

    Implement style on active link

--- webapp/components/sidebar/ListItem.vue
+++ webapp/components/sidebar/ListItem.vue
@@ -2,7 +2,7 @@
   <div class="mb-4 duration-200 ease-out hover:bg-slate-300">
     <NuxtLink
       :to="`/dashboard/${label}`"
-      class="flex h-10 cursor-pointer items-center gap-4 pl-6">
+      class="flex h-10 cursor-pointer items-center gap-8 pl-6">
       <i :class="iconClass"></i>
       <p :class="{ hidden: !isExpanded }">{{ label }}</p>
     </NuxtLink>
@@ -16,3 +16,12 @@ const props = defineProps({
   isExpanded: { type: Boolean, required: true }
 });
 </script>
+
+<style>
+.router-link-active {
+  background-color: rgb(214, 214, 214);
+  border-right: rgb(73, 73, 72);
+  border-right-width: 0.5rem;
+  border-right-style: solid;
+}
+</style>
