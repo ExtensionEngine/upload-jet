0071ae92b0baa13c05197ecf4bccc37e3198a2de
Marko1310 <marko.cabo13@gmail.com>
2023-08-21 17:28:38 +0200

    Add lowercase for nested links

--- webapp/components/sidebar/ListItem.vue
+++ webapp/components/sidebar/ListItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mb-4 duration-200 ease-out hover:bg-slate-300">
     <NuxtLink
-      :to="`/dashboard/${label}`"
+      :to="`/dashboard/${label.toLowerCase()}`"
       class="flex h-10 cursor-pointer items-center gap-8 pl-6">
       <i :class="iconClass"></i>
       <p :class="{ hidden: !isExpanded }">{{ label }}</p>
