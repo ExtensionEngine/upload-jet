c2587521956bb199f207a3426e65fbef1e35b1ec
Petar Trutanic <ptrutanic@extensionengine.com>
2023-08-22 14:45:29 +0200

    Added github client id to env file

--- webapp/nuxt.config.ts
+++ webapp/nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  devServer: {
+    port: 8080
+  },
+  runtimeConfig: {
+    public: {
+      githubClientId: process.env.GITHUB_CLIENT_ID
+    }
+  }
 });
