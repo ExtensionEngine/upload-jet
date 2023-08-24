869cd02708fd600ecf94adbf1ab3a0d5d99d9e3d
Petar Trutanic <ptrutanic@extensionengine.com>
2023-08-24 16:32:20 +0200

    Replaced href with path

--- webapp/components/Login.vue
+++ webapp/components/Login.vue
@@ -4,7 +4,7 @@ const route = useRoute();
 
 const authConfig = {
   scope: 'user:email',
-  state: route.href,
+  state: route.path,
   client_id: config.public.githubClientId
 };
 const searchParams = new URLSearchParams(authConfig).toString();
