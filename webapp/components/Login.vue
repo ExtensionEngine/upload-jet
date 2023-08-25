ec94330a134d00ca42dc629f6a8059ec1ba3b623
Petar Trutanic <ptrutanic@extensionengine.com>
2023-08-25 13:37:04 +0200

    Added login button styling

--- webapp/components/Login.vue
+++ webapp/components/Login.vue
@@ -14,5 +14,23 @@ githubLoginUrl.search = searchParams;
 </script>
 
 <template>
-  <a :href="githubLoginUrl.href">Login with GitHub</a>
+  <a class="github-login" :href="githubLoginUrl.href">Login with GitHub</a>
 </template>
+
+<style scoped>
+.github-login {
+  cursor: pointer;
+  outline: 0;
+  color: #fff;
+  background-color: #0d6efd;
+  border-color: #0d6efd;
+  display: inline-block;
+  font-weight: 400;
+  line-height: 1.5;
+  text-align: center;
+  border: 1px solid transparent;
+  padding: 6px 12px;
+  font-size: 16px;
+  border-radius: 0.25rem;
+}
+</style>
