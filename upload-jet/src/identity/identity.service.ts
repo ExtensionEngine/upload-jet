c7b54aa6fced104e5b6a2a3ae779b939299b326e
Petar Trutanic <ptrutanic@extensionengine.com>
2023-08-28 14:49:43 +0200

    Added upsert on user login

--- upload-jet/src/identity/identity.service.ts
+++ upload-jet/src/identity/identity.service.ts
@@ -1,4 +1,4 @@
-import { EntityRepository } from '@mikro-orm/core';
+import { EntityManager, EntityRepository } from '@mikro-orm/core';
 import { InjectRepository } from '@mikro-orm/nestjs';
 import { Injectable } from '@nestjs/common';
 import Identity from './identity.entity';
@@ -7,13 +7,33 @@ import { GithubProviderService } from 'identity/github-provider.service';
 @Injectable()
 export class IdentityService {
   constructor(
+    private readonly em: EntityManager,
     private readonly githubProvider: GithubProviderService,
     @InjectRepository(Identity)
-    private readonly IdentityRepository: EntityRepository<Identity>
+    private readonly identityRepository: EntityRepository<Identity>
   ) {}
 
   async authorize(code: string) {
     const user = await this.githubProvider.getUser(code);
-    // TODO: Upsert user to the database
+
+    if (user) {
+      const identityEntity = new Identity(user.id, user.email, user.avatarUrl);
+      this.upsert(identityEntity);
+    }
+  }
+
+  async upsert(user: Identity) {
+    const existingUser = await this.identityRepository.findOne({
+      githubId: user.githubId
+    });
+
+    if (existingUser) {
+      existingUser.email = user.email;
+      existingUser.avatarUrl = user.avatarUrl;
+    } else {
+      this.em.persist(user);
+    }
+
+    await this.em.flush();
   }
 }
