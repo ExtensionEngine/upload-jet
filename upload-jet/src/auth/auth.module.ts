af8d49dd9a43de1d806f27eb5d83736cacc87b16
Marko1310 <marko.cabo13@gmail.com>
2023-08-31 21:30:11 +0200

    Remove authorization test controller

--- upload-jet/src/auth/auth.module.ts
+++ upload-jet/src/auth/auth.module.ts
@@ -1,13 +1,12 @@
 import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthorizationController } from './authorization.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { JwtMiddleware } from './jwt.middleware';
 import { PermissionService } from './permission.service';
 
 @Module({
-  controllers: [AuthorizationController],
+  controllers: [],
   providers: [AuthService, PermissionService],
   imports: [
     JwtModule.registerAsync({
