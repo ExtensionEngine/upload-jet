76fe80693443d0240f87d142b662da25bad73f3a
Marko1310 <marko.cabo13@gmail.com>
2023-09-01 11:27:32 +0200

    Create basic modal window

--- webapp/pages/dashboard/applications/index.vue
+++ webapp/pages/dashboard/applications/index.vue
@@ -1,6 +1,21 @@
 <template>
   <button
+    @click="showModal = true"
     class="absolute right-4 top-4 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-600">
     Create App
   </button>
+
+  <Teleport to="body">
+    <Modal :show="showModal" @close="showModal = false">
+      <template>
+        <h3>Modal</h3>
+      </template>
+    </Modal>
+  </Teleport>
 </template>
+
+<script setup>
+import Modal from './modal.vue';
+
+const showModal = ref(false);
+</script>
---
+++ webapp/pages/dashboard/applications/modal.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps({
+  show: { type: Boolean }
+});
+const emit = defineEmits(['close']);
+</script>
+
+<template>
+  <Transition name="modal">
+    <div
+      v-if="show"
+      class="fixed left-0 top-0 z-50 flex h-screen w-screen bg-gray-700 opacity-50 transition-opacity duration-100 ease-in-out">
+      <div class="m-auto w-80 border-r-2 bg-white p-5 pl-7 shadow-md">
+        <div class="modal-footer">
+          <slot name="button">
+            <button class="float-right" @click="emit('close')">Create</button>
+          </slot>
+        </div>
+      </div>
+    </div>
+  </Transition>
+</template>
