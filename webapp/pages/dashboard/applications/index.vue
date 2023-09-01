25b8153139872d68f507e90bfb0727ffac932b9e
Marko1310 <marko.cabo13@gmail.com>
2023-09-01 15:20:26 +0200

    Add a list of apps

--- webapp/pages/dashboard/applications/index.vue
+++ webapp/pages/dashboard/applications/index.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     @click="showModal = true"
-    class="absolute rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-600"
+    class="absolute rounded-lg bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-600"
     :class="
       isDesktop
         ? 'right-4 top-4'
@@ -9,6 +9,14 @@
     ">
     Create App
   </button>
+
+  <div class="mt-20 p-10">
+    <li
+      class="mb-4 flex h-14 list-none items-center rounded-lg border-2 bg-slate-50 pl-4 duration-200 ease-out hover:scale-105 hover:cursor-pointer hover:border-cyan-500"
+      v-for="app in mockedApps">
+      {{ app }}
+    </li>
+  </div>
 
   <Teleport to="body">
     <Modal :show="showModal" @close="showModal = false">
@@ -27,4 +35,11 @@ const showModal = ref(false);
 const props = defineProps({
   isDesktop: { type: Boolean }
 });
+
+const mockedApps = ref([
+  'Mocked App 1',
+  'Mocked App 2',
+  'Mocked App 3',
+  'Mocked App 4'
+]);
 </script>
