5872c60eb1e3359c821134e29ce864b74a566963
Marko1310 <marko.cabo13@gmail.com>
2023-09-01 12:38:59 +0200

    Add title and input to modal, add input value, prevent button click if input value empty

--- webapp/pages/dashboard/applications/modal.vue
+++ webapp/pages/dashboard/applications/modal.vue
@@ -3,17 +3,45 @@ const props = defineProps({
   show: { type: Boolean }
 });
 const emit = defineEmits(['close']);
+
+const inputValue = ref('');
 </script>
 
 <template>
   <Transition name="modal">
     <div
       v-if="show"
-      class="fixed left-0 top-0 z-50 flex h-screen w-screen bg-gray-700 opacity-50 transition-opacity duration-100 ease-in-out">
-      <div class="m-auto w-80 border-r-2 bg-white p-5 pl-7 shadow-md">
+      class="fixed left-0 top-0 z-50 flex h-screen w-screen bg-gray-700 bg-opacity-50 transition-opacity duration-100 ease-in-out">
+      <div
+        class="m-auto flex w-96 flex-col gap-7 rounded-xl bg-white p-5 pl-7 shadow-md">
+        <div class="modal-header">
+          <slot name="header">
+            <h2 class="text-center text-xl uppercase">Create Application</h2>
+          </slot>
+        </div>
+        <div class="modal-input">
+          <slot name="input">
+            <form>
+              <label for="appname"></label>
+              <input
+                v-model="inputValue"
+                type="text"
+                id="appname"
+                name="appname"
+                placeholder="Enter you Application name"
+                class="h-10 w-full border-2 p-2" />
+            </form>
+          </slot>
+        </div>
         <div class="modal-footer">
           <slot name="button">
-            <button class="float-right" @click="emit('close')">Create</button>
+            <button
+              class="float-right"
+              :class="inputValue ? 'text-green-600' : 'text-red-500'"
+              :disabled="!inputValue"
+              @click="emit('close'), (inputValue = '')">
+              Create
+            </button>
           </slot>
         </div>
       </div>
