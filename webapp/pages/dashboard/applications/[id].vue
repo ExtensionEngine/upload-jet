ee61e39136c79263dc0bfcf3a877f0a77d96e764
Petar Trutanic <ptrutanic@extensionengine.com>
2023-09-07 12:00:24 +0200

    Added application details

--- webapp/pages/dashboard/applications/[id].vue
+++ webapp/pages/dashboard/applications/[id].vue
@@ -1,8 +1,22 @@
 <template>
-  <h1>Application</h1>
-  <div>
-    <div>{{ application?.id }}</div>
-    <div>{{ application?.name }}</div>
+  <div class="m-4">
+    <div class="flex justify-between mb-4">
+      <h1 class="text-3xl">Application details</h1>
+      <button class="rounded-lg bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-600">
+        Generate API key
+      </button>
+    </div>
+
+    <div v-if="application">
+      <div class="flex text-xl">
+        <label class="mr-2">Name:</label>
+        <div>{{ application.name }}</div>
+      </div>
+      <div class="flex text-xl">
+        <label class="mr-2">Created:</label>
+        <div>{{ formatDate(application.createdAt) }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,6 +27,7 @@ const route = useRoute();
 interface Application {
   id: number;
   name: string;
+  createdAt: string;
 }
 
 const application = ref<Application | null>(null);
@@ -27,6 +42,10 @@ const fetchApplication = async (id: number) => {
     application.value = data as Application;
   })
 };
+
+const formatDate = (dateString: string) => {
+  return new Date(dateString).toUTCString()
+}
 
 const applicationId = parseInt(route.params.id.toString());
 if (!isNaN(applicationId)) {
