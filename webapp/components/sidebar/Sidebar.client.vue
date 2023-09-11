d0dff687d9d456da21494dbb06527ef0d6c01893
Ivo Kovacevic <ikovacevic96k@gmail.com>
2023-09-11 12:48:48 +0200

    Layout refactors, add breakpoint composables

---
+++ webapp/components/sidebar/Sidebar.client.vue
@@ -0,0 +1,28 @@
+<template>
+  <aside
+    class="fixed flex h-screen flex-col overflow-hidden border-2 border-gray-300 bg-slate-50 duration-500 ease-out"
+    :class="{
+      'w-20': isMobile && !isOpen,
+      'w-80': (isMobile && isOpen) || isDesktop,
+      relative: isDesktop
+    }">
+    <Icon
+      :name="isOpen ? 'mdi:arrow-left' : 'mdi:menu'"
+      size="32"
+      :visibility="isMobile ? 'visible' : 'hidden'"
+      class="z-50 justify-end"
+      :class="{
+        'absolute left-5 cursor-pointer lg:left-6': !isOpen,
+        'absolute right-1 duration-200 ease-out hover:-translate-x-1 hover:cursor-pointer':
+          isOpen
+      }"
+      @click="toggle" />
+    <SidebarUser :is-expanded="isOpen" />
+    <SidebarApplicationList :is-expanded="isOpen" />
+  </aside>
+</template>
+
+<script setup>
+const { isMobile, isDesktop } = useBreakpoint();
+const { isOpen, toggle } = useSidebarMenu();
+</script>
--- webapp/layouts/DashboardLayout.vue
+++ webapp/layouts/DashboardLayout.vue
@@ -1,6 +1,17 @@
+<script setup lang="ts">
+const { isDesktop } = useBreakpoint();
+const cls = computed(() => {
+  return isDesktop.value ? 'pl-0' : 'pl-20';
+});
+</script>
+
 <template>
-  <div class="flex h-screen flex-row">
-    <slot name="sidebar" />
-    <slot name="maincontent" />
+  <div class="flex h-screen">
+    <Sidebar />
+    <div class="flex-1" :class="[cls]">
+      <slot></slot>
+      <div>isDesktop: {{ isDesktop }}</div>
+      <div>cls: {{ cls }}</div>
+    </div>
   </div>
 </template>
