ad20cae91676fc8df0c36b26890893cfb62348b3
Marko1310 <marko.cabo13@gmail.com>
2023-09-13 13:15:47 +0200

    Header change from absolute to fixed

--- webapp/layouts/DashboardLayout.vue
+++ webapp/layouts/DashboardLayout.vue
@@ -6,7 +6,7 @@ const { isOpen, close, toggle } = useSidebarMenu();
   <Header
     @toggle-sidebar="toggle"
     :is-open="isOpen"
-    class="absolute md:hidden md:h-16" />
+    class="fixed top-0 md:hidden md:h-16" />
   <div class="flex">
     <Sidebar @close="close" :is-open="isOpen" />
     <main class="h-screen w-screen pt-12 md:flex-1 md:pt-0">
