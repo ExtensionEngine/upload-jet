39fb3b604a47a062f52c3f3fcf76f0b587c6f3d7
Petar Trutanic <ptrutanic@extensionengine.com>
2023-09-21 23:55:28 +0200

    Removed validation service

--- upload-jet/src/shared/zod-exception.filter.ts
+++ upload-jet/src/shared/zod-exception.filter.ts
@@ -1,25 +1,27 @@
-import {
-  ExceptionFilter,
-  Catch,
-  ArgumentsHost,
-  HttpException
-} from '@nestjs/common';
+import { ExceptionFilter, Catch, ArgumentsHost } from '@nestjs/common';
 import { Response } from 'express';
 import { ZodError } from 'zod';
-import { ValidationService } from './validation.service';
 import { logger } from '@mikro-orm/nestjs';
 
 const BAD_REQUEST_CODE = 400;
 
 @Catch(ZodError)
 export class ZodExceptionFilter implements ExceptionFilter {
-  constructor(private readonly validationService: ValidationService) {}
+  constructor() {}
+
+  mapZodError(error: ZodError) {
+    return error.issues.map(({ path, message, code }) => ({
+      path,
+      message,
+      code
+    }));
+  }
 
   catch(exception: ZodError, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
 
-    const error = this.validationService.mapZodError(exception);
+    const error = this.mapZodError(exception);
     logger.error(error);
 
     response.status(BAD_REQUEST_CODE).json({
