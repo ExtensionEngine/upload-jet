8ffda702e79dbbc97fc98ce527fcc6240413dbe6
Petar Trutanic <ptrutanic@extensionengine.com>
2023-09-25 15:07:33 +0200

    Fixed application layout

--- webapp/pages/dashboard/applications/[id].vue
+++ webapp/pages/dashboard/applications/[id].vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="p-4">
+  <div class="p-4" v-if="application">
     <div class="flex justify-between mb-4">
-      <h1 class="text-3xl">{{ application?.name }}</h1>
+      <h1 class="text-3xl">{{ application.name }}</h1>
       <button class="rounded-lg bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-600">
         Generate API key
       </button>
@@ -10,7 +10,7 @@
     <div>
       <div class="flex text-xl">
         <label class="mr-2">Name:</label>
-        <div>{{ application?.name }}</div>
+        <div>{{ application.name }}</div>
       </div>
       <div class="flex text-xl">
         <label class="mr-2">Created:</label>
@@ -23,19 +23,23 @@
 <script setup lang="ts">
 import { Application } from '@/types/application';
 
+definePageMeta({
+  name: 'Application details',
+  layout: 'dashboard-layout',
+  middleware: ['auth']
+});
+
 const formatDate = (dateString: string | undefined): string => {
   return dateString ? new Date(dateString).toUTCString() : '';
 }
 
 const application: Ref<Application | null> = useState('application', () => null);
 
-if (!application.value) {
-  const { data, error } = await useApiFetch<Application>(`applications/${useRoute().params.id}`);
-  application.value = data.value;
+const { data, error } = await useApiFetch<Application>(`applications/${useRoute().params.id}`);
+application.value = data.value;
 
-  if (error.value) {
-    throw createError({ ...error.value, fatal: true });
-  }
+if (error.value) {
+  throw createError({ ...error.value, fatal: true });
 }
 
 const createdAt = computed(() => {
