49fdedd761a9581e4af6e85c68a9632850055606
Petar Trutanic <ptrutanic@extensionengine.com>
2023-10-02 12:44:18 +0200

    Removed ui lib

--- webapp/components/Avatar.vue
+++ webapp/components/Avatar.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="p-3 flex items-center md:border-b-2 md:border-gray-300">
     <img :src="user?.avatarUrl" class="h-20 w-20 rounded-full border-2 border-gray-300">
-    <UTooltip :text="user?.email">
+    <div :text="user?.email">
       <div class="ml-3 w-36 overflow-hidden overflow-ellipsis">{{ username }}</div>
-    </UTooltip>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
