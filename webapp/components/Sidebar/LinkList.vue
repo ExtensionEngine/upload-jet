82b0eec1187b0631a4bff8a4c1433eac4535b291
Petar Trutanic <ptrutanic@extensionengine.com>
2023-10-03 09:37:15 +0200

    Formatting

--- webapp/components/Sidebar/LinkList.vue
+++ webapp/components/Sidebar/LinkList.vue
@@ -1,6 +1,10 @@
 <template>
   <nav class="flex flex-col space-y-2">
-    <NuxtLink v-for="route in menu" @click="$emit('close')" :to="route.to" :key="route.text"
+    <NuxtLink
+      v-for="route in menu"
+      @click="$emit('close')"
+      :to="route.to"
+      :key="route.text"
       class="nav-link p-4 duration-200 ease-out hover:bg-slate-300">
       <Icon :name="route.icon" size="24" class="mr-4" />
       {{ route.text }}
