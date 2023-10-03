594d81ffacaee406b94dddef00913358a437bccd
Petar Trutanic <ptrutanic@extensionengine.com>
2023-10-03 08:06:33 +0200

    Updated user email display

--- webapp/components/Avatar.vue
+++ webapp/components/Avatar.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="p-3 flex items-center md:border-b-2 md:border-gray-300">
     <img :src="user?.avatarUrl" class="h-20 w-20 rounded-full border-2 border-gray-300">
-    <div :text="user?.email">
-      <div class="ml-3 w-36 overflow-hidden overflow-ellipsis">{{ username }}</div>
-    </div>
+    <div class="ml-3 w-36 break-words">{{ user?.email }}</div>
   </div>
 </template>
 <script setup lang="ts">
