a3aa1bf0fe00c4257ff5bf190998a76e19afb0a3
Petar Trutanic <ptrutanic@extensionengine.com>
2023-10-05 09:48:51 +0200

    Application key cascade on application delte

--- upload-jet/src/shared/database/migrations/20231004063203-application-id-cascade.ts
+++ upload-jet/src/shared/database/migrations/20231004063203-application-id-cascade.ts
@@ -3,12 +3,16 @@ import { Migration } from '@mikro-orm/migrations';
 const TABLE_NAME = 'api_key';
 const COLUMN_NAME = 'application_id';
 
-export class ApplicationIdNullable extends Migration {
+export class ApplicationIdCascade extends Migration {
   async up(): Promise<void> {
     const knex = this.getKnex();
 
     const schema = knex.schema.alterTable(TABLE_NAME, table => {
-      table.integer(COLUMN_NAME).nullable().alter();
+      table.dropForeign(COLUMN_NAME);
+      table
+        .foreign(COLUMN_NAME)
+        .references('application.id')
+        .onDelete('CASCADE');
     });
 
     this.addSql(schema.toQuery());
@@ -18,7 +22,11 @@ export class ApplicationIdNullable extends Migration {
     const knex = this.getKnex();
 
     const schema = knex.schema.alterTable(TABLE_NAME, table => {
-      table.integer(COLUMN_NAME).notNullable().alter();
+      table.dropForeign(COLUMN_NAME);
+      table
+        .foreign(COLUMN_NAME)
+        .references('application.id')
+        .onDelete('NO ACTION');
     });
 
     this.addSql(schema.toQuery());
