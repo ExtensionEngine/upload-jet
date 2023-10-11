eb102b80ac7969a5b56158dab188a46ea9ad5034
Petar Trutanic <ptrutanic@extensionengine.com>
2023-10-11 10:08:30 +0200

    Added api key validation

--- upload-jet/src/file/file.module.ts
+++ upload-jet/src/file/file.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { FileController } from './file.controller';
 import { FileService } from './file.service';
 import { S3ClientService } from 'upload-policy/s3-client.service';
+import { ApiKeyValidationMiddleware } from 'application/api-key-validation.middleware';
+import { ApplicationModule } from 'application/application.module';
 
 @Module({
   controllers: [FileController],
-  providers: [FileService, S3ClientService]
+  providers: [FileService, S3ClientService],
+  imports: [ApplicationModule]
 })
-export class FileModule {}
+export class FileModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ApiKeyValidationMiddleware).forRoutes('*');
+  }
+}
